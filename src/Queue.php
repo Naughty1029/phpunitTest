bb47d253920d7907123efac863ae8034d96dc2ea
Naughty1029 <masahiro.watanabe02@gmail.com>
2025-01-02 16:42:50 +0900

    例外のテスト: コードが例外を投げることを期待する

--- src/Queue.php
+++ src/Queue.php
@@ -3,15 +3,24 @@
 final class Queue
 {
   /**
+   * @var int
+   */
+  public const MAX_ITEMS = 5;
+
+  /**
    * @var array
    */
   protected $items = [];
 
   /**
    * @param mixed
+   * @throws QueueException
    */
   public function push($item)
   {
+    if($this->getCount() == static::MAX_ITEMS){
+      throw new QueueException("Queue is full");
+    }
     $this->items[] = $item;
   }
 
---
+++ src/QueueException.php
@@ -0,0 +1,5 @@
+<?php
+final class QueueException extends Exception
+{
+  
+}
