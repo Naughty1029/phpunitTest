776ec34e230f8f8f76b1903580f33793f79b6f65
Naughty1029 <masahiro.watanabe02@gmail.com>
2025-01-05 17:07:36 +0900

    依存性注入：クラスが依存するオブジェクトを注入する

--- src/User.php
+++ src/User.php
@@ -13,10 +13,43 @@ class User
   public $surname;
 
   /**
+   * Email address
+   * @var string
+   */
+  public $email;
+
+  /**
+   * Mailer object
+   * @var Mailer
+   */
+  protected $mailer;
+
+  /**
+   * Set the mailer dependency
+   *
+   * @param Mailer $mailer The Mailer object
+   */
+  public function setMailer(Mailer $mailer) {
+    $this->mailer = $mailer;
+  }
+
+  /**
    * @return string
    */
   public function getFullName()
   {
     return trim("$this->first_name $this->surname");
+  }
+
+  /**
+   * Send the user a message
+   *
+   * @param string $message The message
+   *
+   * @return boolean True if sent, false otherwise
+   */
+  public function notify($message)
+  {
+    return $this->mailer->sendMessage($this->email, $message);
   }
 }
