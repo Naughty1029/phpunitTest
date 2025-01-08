6b2db1f864e5fe3030d58003ded2e57d4d0d5bff
Masahiro Watanabe <masahiro.watanabe02@gmail.com>
2025-01-09 08:48:08 +0900

    Mockeryを使ってまだ存在しない依存関係をモックする

--- tests/OrderTest.php
+++ tests/OrderTest.php
@@ -9,6 +9,10 @@ interface PaymentGateway
 
 class OrderTest extends TestCase
 {
+    public function tearDown() : void {
+        Mockery::close();
+    }
+
     public function testOrderIsProcessed()
     {
         $gateway = $this->getMockBuilder('PaymentGateway')
@@ -20,6 +24,21 @@ class OrderTest extends TestCase
                 ->with($this->equalTo(200))
                 ->willReturn(true);
         
+        $order = new Order($gateway);
+
+        $order->amount = 200;
+        
+        $this->assertTrue($order->process());
+    }
+
+    public function testOrderIsProcessedUsingMockery()
+    {
+        $gateway = Mockery::mock('PaymentGateway');
+        $gateway->shouldReceive('charge')
+                ->once()
+                ->with(200)
+                ->andReturn(true);
+    
         $order = new Order($gateway);
 
         $order->amount = 200;
